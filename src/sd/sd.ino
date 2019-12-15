28d74ab2d21f33b4ecd3557ccb26419c44b3b537
quanten <590138-quanten@users.noreply.gitlab.com>
2019-12-15 12:57:22 +0100

    add onebutton libary, refractored a bit

--- src/sd/sd.ino
+++ src/sd/sd.ino
@@ -4,51 +4,67 @@
 #include <SD.h>
 #include <Wire.h>
 #include <SPI.h>
+#include "OneButton.h"
+
+const int buttonPin = 5;
+OneButton button(buttonPin, false);
+byte push;
+
 File myFile_1;
 File myFile_2;
 File myFile_3;
 
-int delayms = 75;
+int delayms = 90;
 
-byte push = 0;
+#define LED_1 3;
+#define LED_2 4
 
-byte led_1 = 3;
-byte led_2 = 4;
+#define CS_PIN 10
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void setup() {
   Serial.begin(115200);
-  pinMode(led_1, OUTPUT);
-  pinMode(led_2, OUTPUT);
+  pinMode(LED_1, OUTPUT);
+  pinMode(LED_2, OUTPUT);
+
+  blinken2(100, 1);
+  blinken(150, 3);
+
+  pinMode(buttonPin, INPUT);
+  button.attachClick(clickedIt);
+  button.attachDoubleClick(doubleClickedIt);
+  button.attachLongPressStart(longPressStart1);
+
   Wire.begin();
   blinken(25, 3);
   delay(50);
-  pinMode(10, OUTPUT); //10 = cs pin
-  if (!SD.begin(10)) { // 10 = cs pin
+
+  pinMode(CS_PIN, OUTPUT);
+  if (!SD.begin(CS_PIN)) {
     Serial.println("SD Karte nicht gefunden");
     while (true) {
       blinken(600, 3);
       //Fehler SD Karte
     }
   }
-
   blinken(150, 3);
 
-  myFile_1 = SD.open("1.c4b", FILE_READ); //file with the ani data. cab = cube 4x4x4 binary ;) because of the 8.3 file names not longer file extension are allowed
+  myFile_1 = SD.open("1.c4b", FILE_READ); //file with the ani data. c4b = cube 4x4x4 binary ;) because of the 8.3 file names not longer file extension are allowed
   if ((myFile_1.size() % 24) != 0) {
     while (true) {
       ; //because than the file bad and have not the rigth length
     }
   }
 
-  myFile_2 = SD.open("2.c4b", FILE_READ); //file with the ani data. cab = cube 4x4x4 binary ;) because of the 8.3 file names not longer file extension are allowed
+  myFile_2 = SD.open("2.c4b", FILE_READ); //file with the ani data. c4b = cube 4x4x4 binary ;) because of the 8.3 file names not longer file extension are allowed
   if ((myFile_2.size() % 24) != 0) {
     while (true) {
       ; //because than the file bad and have not the rigth length
     }
   }
 
-  myFile_3 = SD.open("3.c4b", FILE_READ); //file with the ani data. cab = cube 4x4x4 binary ;) because of the 8.3 file names not longer file extension are allowed
+  myFile_3 = SD.open("3.c4b", FILE_READ); //file with the ani data. c4b = cube 4x4x4 binary ;) because of the 8.3 file names not longer file extension are allowed
   if ((myFile_3.size() % 24) != 0) {
     while (true) {
       ; //because than the file bad and have not the rigth length
@@ -58,11 +74,15 @@ void setup() {
   blinken(500, 1);
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 void loop() {
+
+  button.tick();
+
 
   Wire.beginTransmission(44);
   for (int i = 0; i < 24; i++) {
-    //TODO das if ausserhalb der For-Schleife verschieben, damit die File Pointer immer auf 24 Byte Vielfache zeigt
     if (push == 0) {
       Wire.write(myFile_1.read());
     }
@@ -73,9 +93,11 @@ void loop() {
       Wire.write(myFile_3.read());
     }
   }
-  delay(1); //delay 10 ms that the slave can compute the i2c input and dont overfill the buffer
+
+  delay(1); //delay 1 ms that the slave can compute the i2c input and dont overfill the buffer
 
   int resultint = Wire.endTransmission();
+
   if (resultint != 0) {
     //rum das es nicht geht zB mit LED
     blinken(1, 5);
@@ -88,12 +110,14 @@ void loop() {
       blinken(1, 10);
     }
   }
+
   if (push == 1) {
     if (myFile_2.available() == 0) {
       myFile_2.seek(0);
       blinken(1, 10);
     }
   }
+
   if (push == 255) {
     if (myFile_3.available() == 0) {
       myFile_3.seek(0);
@@ -106,20 +130,41 @@ void loop() {
 
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 void blinken(int del, int n) {
   for (int i = 0; i < n; i++) {
-    digitalWrite(led_2, HIGH);
+    digitalWrite(LED_2, HIGH);
     delay(del);
-    digitalWrite(led_2, LOW);
+    digitalWrite(LED_2, LOW);
     delay(del);
   }
 }
 
 void blinken2(int del2, int n2) {
   for (int i2 = 0; i2 < n2; i2++) {
-    digitalWrite(led_1, HIGH);
+    digitalWrite(LED_1, HIGH);
     delay(del2);
-    digitalWrite(led_1, LOW);
+    digitalWrite(LED_1, LOW);
     delay(del2);
   }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void clickedIt() {
+  blinken2(100, 1);
+  Serial.println("1");
+  push = 0;
+}
+void doubleClickedIt() {
+  blinken2(150, 2);
+  Serial.println("2");
+  push = 1;
+}
+
+void longPressStart1() {
+  blinken2(800, 1);
+  Serial.println("3");
+  push = 255;
 }
